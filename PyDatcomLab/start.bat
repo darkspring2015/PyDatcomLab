af750eaefa45641a3a4baf603c4d5b4c3f373f20
linger zhu <lingo_renlo@qq.com>
2018-01-13 23:36:13 +0800

    查看网页

--- PyDatcomLab/start.bat
+++ PyDatcomLab/start.bat
@@ -1,1 +1,1 @@
-python main.py --ProjectDir="E:\Projects\PyDatcomLab\extras\PyDatcomProjects\1" --log="E:\Projects\PyDatcomLab\extras\PyDatcomProjects\1\log\log.log" 
+C:\Users\linger\Anaconda3\python main.py --ProjectDir="E:\Projects\PyDatcomLab\extras\PyDatcomProjects\1" --log="E:\Projects\PyDatcomLab\extras\PyDatcomProjects\1\log\log.log" 
